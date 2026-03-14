53c13f4a1f15049680c45798fd4d175685eae45c
arthur-mendonca <arthu91@gmail.com>
2026-03-14 20:08:05 -0300

    alterar app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import AddTask from "./components/AddTask.jsx";
 import About from "./components/About.jsx";
 import DebugLogs from "./components/DebugLogs.jsx";
 
-const apiUrl = import.meta.env.VITE_API_URL || window.location.origin;
+const apiUrl = import.meta.env.VITE_API_URL || "";
 
 function AppContent() {
   const [tasks, setTasks] = useState([]);
